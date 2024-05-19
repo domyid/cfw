8df59838d1ad1bffd63dbf352d2c5932398569f3
Rolly Maulana Awangga <awangga@gmail.com>
2024-05-19 15:03:51 +0700

    coba dulu

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/domyid/cfw
 
 go 1.22.3
+
+require github.com/syumai/workers v0.26.0
--- main.go
+++ main.go
@@ -1,31 +1,16 @@
 package main
 
 import (
-	"syscall/js"
+	"net/http"
+
+	"github.com/syumai/workers"
 )
 
-func handleRequest(this js.Value, args []js.Value) interface{} {
-	url := args[1].String() // Ambil URL dari argumen kedua
-	req := args[0]
-	method := req.Get("method").String()
-
-	opts := map[string]interface{}{
-		"method": method,
-		// Tambahkan headers dan body jika diperlukan
-	}
-
-	promise := js.Global().Get("fetch").Invoke(url, opts)
-	then := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-		response := args[0]
-		return response
-	})
-	defer then.Release()
-
-	promise.Call("then", then)
-	return nil
+func handleRequest(req *http.Request) *http.Response {
+	// Your code here
+	return workers.NewResponse("Hello, World!", nil)
 }
 
 func main() {
-	js.Global().Set("handleRequest", js.FuncOf(handleRequest))
-	select {}
+	workers.HandleRequest(handleRequest)
 }
